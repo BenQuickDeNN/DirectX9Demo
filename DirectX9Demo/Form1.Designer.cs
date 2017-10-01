39c50b0b4668ea05695e20cb31d2f55674874e13
BenQuickDeNN <2413269761@qq.com>
2017-10-01 21:17:32 +0800

    可显示静态模型

--- DirectX9Demo/Form1.Designer.cs
+++ DirectX9Demo/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip_Main = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem_File = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_File_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog_XFile = new System.Windows.Forms.OpenFileDialog();
+            this.panel_Disp = new System.Windows.Forms.Panel();
+            this.menuStrip_Main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip_Main
+            // 
+            this.menuStrip_Main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_File});
+            this.menuStrip_Main.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip_Main.Name = "menuStrip_Main";
+            this.menuStrip_Main.Size = new System.Drawing.Size(784, 25);
+            this.menuStrip_Main.TabIndex = 0;
+            this.menuStrip_Main.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem_File
+            // 
+            this.toolStripMenuItem_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_File_Open});
+            this.toolStripMenuItem_File.Name = "toolStripMenuItem_File";
+            this.toolStripMenuItem_File.Size = new System.Drawing.Size(44, 21);
+            this.toolStripMenuItem_File.Text = "文件";
+            // 
+            // toolStripMenuItem_File_Open
+            // 
+            this.toolStripMenuItem_File_Open.Name = "toolStripMenuItem_File_Open";
+            this.toolStripMenuItem_File_Open.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem_File_Open.Text = "打开...";
+            this.toolStripMenuItem_File_Open.Click += new System.EventHandler(this.MenuItemClickEventHandler);
+            // 
+            // openFileDialog_XFile
+            // 
+            this.openFileDialog_XFile.Filter = "(*.x)|*.x";
+            // 
+            // panel_Disp
+            // 
+            this.panel_Disp.BackColor = System.Drawing.Color.White;
+            this.panel_Disp.Location = new System.Drawing.Point(13, 29);
+            this.panel_Disp.Name = "panel_Disp";
+            this.panel_Disp.Size = new System.Drawing.Size(759, 520);
+            this.panel_Disp.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel_Disp);
+            this.Controls.Add(this.menuStrip_Main);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip_Main;
+            this.Name = "Form1";
+            this.Text = "X Static Viewer";
+            this.menuStrip_Main.ResumeLayout(false);
+            this.menuStrip_Main.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip_Main;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_File;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_File_Open;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_XFile;
+        private System.Windows.Forms.Panel panel_Disp;
     }
 }
 
--- DirectX9Demo/Form1.cs
+++ DirectX9Demo/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +13,130 @@ namespace DirectX9Demo
 {
     public partial class Form1 : Form
     {
+        #region DirectX9
+        Mesh XObjectMesh = null;// X文件网格
+        Device XDevice = null;// X显示设备
+        Material[] XObjectMeshMaterials;// X文件材质
+        Texture[] XObjectMeshTextures;// X文件纹理
+
+        /// <summary>
+        /// 初始化图形设备
+        /// </summary>
+        void InitializeGraphics()
+        {
+            PresentParameters XPresentParam = new PresentParameters();
+            XPresentParam.Windowed = true;// 窗口模式运行
+            // 设置交换效果
+            XPresentParam.SwapEffect = SwapEffect.Discard;
+            XPresentParam.AutoDepthStencilFormat = DepthFormat.D16;
+            XPresentParam.EnableAutoDepthStencil = true;
+            // 创建设备
+            XDevice = new Device(0, DeviceType.Hardware, panel_Disp, CreateFlags.SoftwareVertexProcessing, XPresentParam);
+
+        }
+        /// <summary>
+        /// 加载网格文件
+        /// </summary>
+        /// <param name="filePath"></param>
+        void LoadMesh(string filePath)
+        {
+            if (XDevice == null) return;
+            ExtendedMaterial[] XExtMaterials = null;
+            try
+            {
+                XObjectMesh = Mesh.FromFile(filePath, MeshFlags.Managed, XDevice, out XExtMaterials);
+                // 如果有材质就载入
+                if (XExtMaterials != null && XExtMaterials.Length > 0)
+                {
+                    // 加载材质和纹理
+                    XObjectMeshMaterials = new Material[XExtMaterials.Length];
+                    XObjectMeshTextures = new Texture[XExtMaterials.Length];
+                    for (int i = 0; i < XExtMaterials.Length; i++)
+                    {
+                        XObjectMeshMaterials[i] = XExtMaterials[i].Material3D;
+                        if (XExtMaterials[i].TextureFilename != null && XExtMaterials[i].TextureFilename != string.Empty)
+                        {
+                            XObjectMeshTextures[i] = TextureLoader.FromFile(XDevice, filePath + XExtMaterials[i].TextureFilename);
+                        }
+                    }
+                }
+            }
+            catch (Direct3DXException D3DXe)
+            {
+                MessageBox.Show(D3DXe.ToString());
+            }
+            finally
+            {
+            }
+        }
+        /// <summary>
+        /// 绘制模型
+        /// </summary>
+        /// <param name="yaw"></param>
+        /// <param name="pitch"></param>
+        /// <param name="roll"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        void DrawMesh(float yaw, float pitch, float roll, float x, float y, float z)
+        {
+            if (XDevice == null) return;
+            XDevice.Transform.World = Matrix.RotationYawPitchRoll(yaw, pitch, roll) * Matrix.Translation(x, y, z);
+            // 绘制
+            for (int i = 0; i < XObjectMeshMaterials.Length; i++)
+            {
+                XDevice.Material = XObjectMeshMaterials[i];
+                XDevice.SetTexture(0, XObjectMeshTextures[i]);
+                XObjectMesh.DrawSubset(i);
+            }
+        }
+        /// <summary>
+        /// 设置视角与灯光
+        /// </summary>
+        void SetCamera()
+        {
+            if (XDevice == null) return;
+            XDevice.Transform.Projection = Matrix.PerspectiveFovLH((float)Math.PI / 12, this.Width / this.Height, 0.80f, 10000.0f);
+            XDevice.Transform.View = Matrix.LookAtLH(new Vector3(-400, -100, 600.0f), new Vector3(), new Vector3(0, 1, 0));
+            XDevice.RenderState.Ambient = Color.Black;
+            XDevice.Lights[0].Type = LightType.Directional;
+            XDevice.Lights[0].Diffuse = Color.AntiqueWhite;
+            XDevice.Lights[0].Direction = new Vector3(0, 1, 0);
+            XDevice.Lights[0].Update();
+            XDevice.Lights[0].Enabled = true;
+        }
+        #endregion
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        /// <summary>
+        /// 菜单项点击处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void MenuItemClickEventHandler(object sender, EventArgs args)
+        {
+            if (sender.Equals(toolStripMenuItem_File_Open)) menuItem_OpenFile_Click();
+        }
+        void menuItem_OpenFile_Click()
+        {
+            if (openFileDialog_XFile.ShowDialog() == DialogResult.OK)
+            {
+                if (XDevice == null) InitializeGraphics();
+                
+                LoadMesh(openFileDialog_XFile.FileName);
+                XDevice.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.White, 1.0f, 1);
+                SetCamera();
+                XDevice.Present();
+                XDevice.BeginScene();
+                float angle = 0.0f;
+                DrawMesh(angle / (float)Math.PI, angle / (float)Math.PI * 2.0f, angle / (float)Math.PI / 10.0f, 0.0f, -40.0f, 0.0f);
+                XDevice.EndScene();
+
+                XDevice.Present();
+            }
         }
     }
 }
